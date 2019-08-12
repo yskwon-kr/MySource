9b0bca1cbef82ad8c8c24f7301937f9485168523
yskwon-kr <leftfootone@gmail.com>
2019-08-13 01:14:20 +0900

    commit test 7

--- src/main/java/com/jwgye/bcm/new_test.java
+++ src/main/java/com/jwgye/bcm/new_test.java
@@ -7,5 +7,6 @@ public class new_test {
   String test3 ="test3";
   String test4 ="test4";
   String test5 ="test5";
-   String test5 ="test6";
+   String test6 ="test6";
+   String test7 ="test7";
 }
